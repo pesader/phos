93f43b03fa13af4255101b1825fee1e328a2b731
pesader <pesader@users.noreply.github.com>
2024-02-04 00:44:19 -0300

    custom: fix awatcher script with correct path

--- config/scripts/install-awatcher.sh
+++ config/scripts/install-awatcher.sh
@@ -8,4 +8,4 @@ set -oue pipefail
 # Your code goes here.
 curl -L -s https://github.com/2e3s/awatcher/releases/latest/download/awatcher-bundle.zip -o awatcher-bundle.zip
 unzip awatcher-bundle.zip
-mv awatcher-bundle/awatcher /usr/bin/awatcher
+mv awatcher /usr/bin/
