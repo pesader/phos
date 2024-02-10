a0b755fff862a10e4e552135687085aec5576c97
pesader <pesader@users.noreply.github.com>
2024-02-09 23:59:06 -0300

    custom: add fzf, with its configurations

---
+++ config/files/usr/etc/profile.d/fzf.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# zsh
+if echo $SHELL | grep -q "zsh"; then
+    source /usr/share/fzf/shell/key-bindings.zsh
+
+# bash
+elif echo $SHELL | grep -q "bash"; then
+    source /usr/share/fzf/shell/key-bindings.bash
+
+fi
+
