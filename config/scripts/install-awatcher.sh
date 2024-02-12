a0134930152c24bfad4429ca444f92ece9b960b3
pesader <pesader@users.noreply.github.com>
2024-02-12 18:48:37 -0300

    custom: pin awatcher to version 0.2.0

--- config/scripts/install-awatcher.sh
+++ config/scripts/install-awatcher.sh
@@ -6,6 +6,10 @@
 set -oue pipefail
 
 # Your code goes here.
-curl -L -s https://github.com/2e3s/awatcher/releases/latest/download/awatcher-bundle.zip -o awatcher-bundle.zip
+version=0.2.0
+
+curl -L -s "https://github.com/2e3s/awatcher/releases/download/v${version}/awatcher-bundle.zip" -o awatcher-bundle.zip
 unzip awatcher-bundle.zip
 mv awatcher /usr/bin/
+
+unset version
