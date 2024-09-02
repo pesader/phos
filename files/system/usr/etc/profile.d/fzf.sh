0d409ae9382080b26275ab28c112a82013942a1f
Pedro Sader Azevedo <pedro.saderazevedo@pm.me>
2024-09-02 17:58:48 -0300

    fzf: update config options

--- files/system/usr/etc/profile.d/fzf.sh
+++ files/system/usr/etc/profile.d/fzf.sh
@@ -1,2 +1,5 @@
-shell=$(basename $(echo $SHELL))
-source "/usr/share/fzf/shell/key-bindings.$shell"
+source <(fzf --zsh)
+export FZF_DEFAULT_OPTS=" \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
